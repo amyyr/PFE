2bc0de608da1f23f12077ec9756c675353fb7e86
Amyyr <amir.lakhdhar@esprit.tn>
2024-06-14 13:32:44 +0100

    statistic chatreha tagd mazlt bech nzidou el actioon

--- src/app/statistic/statistic.component.ts
+++ src/app/statistic/statistic.component.ts
@@ -1,26 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { MatchService } from '../service/match.service';
 import { PlayerService } from '../service/player.service';
-
 
 @Component({
   selector: 'app-statistic',
   templateUrl: './statistic.component.html',
   styleUrls: ['./statistic.component.css']
 })
-export class StatisticComponent {
+export class StatisticComponent implements OnInit {
   ActionsTeamHome = ["Tir", "assist", "but", "dribble", "centrage", "passe", "keyPasse", "interceptions", "degagements", "fautes"];
   ActionsTeamAway = ["Tir", "assist", "but", "dribble", "centrage", "passe", "keyPasse", "interceptions", "degagements", "fautes"];
   players: any[] = [];
 
-  // Initialize players as an empty array
- 
-
-  matchId: number = 82; // Replace with actual match ID or obtain dynamically
   matches: { id: string, name: string }[] = [];
   teamHome: any[] = [];
   teamAway: any[] = [];
-
 
   selectedMatch = '';
   selectedHomeTeam = '';
@@ -29,52 +23,37 @@ export class StatisticComponent {
   selectedPlayer: any = null;
 
   message = '';
-playerTeamHome: any;
-  
-  constructor(private matchService: MatchService,private playerService: PlayerService) {}
 
-  ngOnInit(): void
+  constructor(private matchService: MatchService, private playerService: PlayerService) {}
 
-   {
-    this.matchService. getMatchByIdNames().subscribe(
+  ngOnInit(): void {
+    this.matchService.getMatchByIdNames().subscribe(
       data => {
         this.matches = Object.keys(data).map(key => ({ id: key, name: data[key] }));
-        console.log('Players loaded:', this.players);
-
       },
       error => {
         console.error('Error fetching matches', error);
         alert('Erreur lors de la récupération des matchs: ' + error.message);
       }
     );
-    const matchId = 82; // Remplacez ceci par l'ID du match que vous souhaitez récupérer
-    this.getPlayers(matchId);
-     }
-
-  
+  }
 
   updateMessage() {
     this.message = `Le joueur ${this.selectedPlayer} fait une action ${this.selectedAction} avec l'équipe ${this.selectedHomeTeam} dans le match ${this.selectedMatch}`;
   }
 
-
-  getPlayers(id: number): void {
-    this.matchService.getPlayersByIdMatch(id).subscribe({
-      next: (data) => {
-        this.players = [...data.playerTeamHome, ...data.playerTeamAway];
-        console.log("les données des joueres sont bien")
-      },
-      error: (error) => {
-        console.error('Error fetching players', error);
-      }
-    });
-  }
-
   onMatchSelected(): void {
     if (this.selectedMatch) {
       const matchId: number = parseInt(this.selectedMatch, 10);
-      this.getPlayers(matchId);
+      this.matchService.getPlayersByIdMatch(matchId).subscribe({
+        next: (data) => {
+          this.teamHome = data.playerTeamHome;
+          this.teamAway = data.playerTeamAway;
+        },
+        error: (error) => {
+          console.error('Error fetching players', error);
+        }
+      });
     }
   }
-  
 }
